7c86cc19db0598bde471976fef1326a4cd4ea348
jn-97 <kjo5395@gmail.com>
2023-11-27 18:44:35 +0900

    polls의 models.py 추가

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+  question_text = models.CharField(max_length=200) # 질문
+  pub_date = models.DateTimeField("date published") # 발행일
+
+  # __str__() => 객체의 표현을 대화식 프롬프트에서 편하게 보기 위해 사용
+  # ex. python manage.py shell -> Question.objects.all()
+  # <QuerySet [<Question: Question object (1)>]> -> <QuerySet [<Question: What's up?>]>
+  def __str__(self):
+    return self.question_text
+  
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+# 각각의 Choice가 하나의 Question에 관계됨
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choice_text = models.CharField(max_length=200) # 선택 텍스트
+  votes = models.IntegerField(default=0) # 투표 집계
+
+  def __str__(self):
+    return self.choice_text
